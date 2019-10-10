386d6fefd7379982b42ad09631ee62580693662c
David Zuzunaga <desarrollo30@farmaciasperuanas.pe>
2019-10-10 16:42:07 -0500

    20191010 Se añadió equals y hashcode a los bean BO

--- 02-repository/src/main/java/com/arquitecturajava/bo/Noticia.java
+++ 02-repository/src/main/java/com/arquitecturajava/bo/Noticia.java
@@ -62,5 +62,30 @@ public class Noticia implements Serializable {
 	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Noticia other = (Noticia) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
 	
 }
